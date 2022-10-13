9fd416724ebb615ba0b1eac6d762dbfdf5498e4c
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-13 17:08:30 +0200

    adding list item with input value

--- add-button.js
+++ add-button.js
@@ -1,2 +1,12 @@
-const toDoButton = document.getElementsByClassName(.toDoButton);
-toDoButton.addEventListener("click", (e) => e.preventDefault();)
+const toDoButton = document.getElementsByClassName('.toDoButton');
+toDoButton.addEventListener("click", (e) => {
+    
+    e.preventDefault();
+    const li=document.createElement("li")
+    li.innerHTML = input.value
+// li.addEventListener('click', (e)=>{
+//     e.target.style.color = black ? e.target.style.color = grey;
+// })
+ol.appendChild(li)
+
+})
