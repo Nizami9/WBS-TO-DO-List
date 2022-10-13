2bca5a9802717f9f7794c3d033648fc9912d83b4
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-13 17:30:57 +0200

    fixing add button

--- add-button.js
+++ add-button.js
@@ -1,13 +1,17 @@
-const toDoButton = document.getElementsByClassName('.toDoButton');
+
+const toDoButton = document.querySelector('.toDoButton');
+const toDoInput =document.querySelector('.toDoInput')
+const toDoList = document.querySelector('.toDoList')
 toDoButton.addEventListener("click", (e) => {
     
     e.preventDefault();
+    console.log(e)
     const li=document.createElement("li")
-    li.innerHTML = input.value
+    li.innerHTML = toDoInput.value
 // li.addEventListener('click', (e)=>{
 //     e.target.style.color = black ? e.target.style.color = grey;
 // })
-ol.appendChild(li)
+toDoList.appendChild(li)
 input.value = ''
 
 })
